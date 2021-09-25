1b8ced6ec6f3e6776b5f12481444f6ab017da457
edwinrainville <erainvil@uw.edu>
2021-09-25 14:45:20 -0700

    Started building the mission report script

--- tools/dailyReport.py
+++
@@ -1,5 +0,0 @@
-## Create Daily Report 
-# Description: This script goes through all data after it has been offloaded and makes a daily report of the data
-# The daily report will include:
-# 1. List of microSWIFTs that were deployed that day
-# 2. A map all drifter tracks - color coded by time 
---
+++ tools/missionReport.py
@@ -0,0 +1,16 @@
+# deploymentReport.py
+'''
+@edwinrainville
+
+Description: This script goes through all data after it has been offloaded and makes a daily report of the data
+
+The daily report will include:
+1. List of microSWIFTs that were deployed that day
+2. A map all drifter tracks - color coded by time 
+
+'''
+
+# Import Statements
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
