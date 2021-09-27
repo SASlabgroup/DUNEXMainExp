1363f87eea5cd0792d7b2a942b2d51e3f2bb06e5
edwinrainville <erainvil@uw.edu>
2021-09-26 18:08:54 -0700

    microSWIFTTools.py now has a spot for both the mission_map function and the mission_accelerations functions

--- tools/microSWIFTTools.py
+++ tools/microSWIFTTools.py
@@ -1,5 +1,13 @@
 # Tools for analyzing data from microSWIFTs
+# Import modules
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal
+
 
 def mission_map():
-    print('This function makes a map of the mission color-coded by time from lat-lon data')
+    return 'This function makes a map of the mission color-coded by time from lat-lon data'
+
+def mission_accels():
+    return 'This function plots a time series of all accelerations '
     
