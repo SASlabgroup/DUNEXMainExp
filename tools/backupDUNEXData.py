99a23e7dbf46f514a757ae9c875b720cc91815f0
edwinrainville <erainvil@uw.edu>
2021-09-27 13:56:32 -0700

    backupDUNEXData.py is fully operational and will be used to backup the data from the local machine to the remote bigwaves.

--- tools/backupDUNEXData.py
+++ tools/backupDUNEXData.py
@@ -1,4 +1,7 @@
-# Automated script to backup all microSWIFT data from local directory to the RAID on Bigwaves
+# Import Statements
+import subprocess
+import logging
+
 def main():
     '''
     @edwinrainville
@@ -7,5 +10,59 @@ def main():
     safe and secure.
     '''
 
+    # Enter Login credentials for bigwaves RAID
+    username = input('Enter bigwaves username: ')
+    password = input('Enter bigwaves password: ')
+
+    # Confirm that you are on the APL VPN
+    answer_val = 1
+    while answer_val != 0:
+        want_to_backup = input('Confirm you are on the APL VPN(yes/no): ')
+
+        if want_to_backup == 'yes':
+            answer_val = 0
+
+        elif want_to_backup == 'no':
+            print('Exiting data backup')
+            return
+
+        else: 
+            print('Enter either yes or no')
+
+    # Confirm that you want to back up data
+    answer_val = 1
+    while answer_val != 0:
+        want_to_backup = input('Confirm you want to back up this data(yes/no): ')
+
+        if want_to_backup == 'yes':
+            print('Data backup is starting')
+            answer_val = 0
+
+        elif want_to_backup == 'no':
+            print('Exiting data backup')
+            return
+
+        else: 
+            print('Enter either yes or no')
+
+    # Define path to data directory
+    microSWIFT_data_dir = '../microSWIFT_data/'
+
+    # Define backup directory on bigwaves RAID
+    bigwaves = 'bigwaves.apl.washington.edu'
+    backup_dir = '/Volumes/Data/DuckFRF/DUNEXMainExp_2021/'
+
+    # Starting actual data backup
+    print('Backing up data in {0} on local machine to {1} on bigwaves'.format(microSWIFT_data_dir, backup_dir))
+
+    # rsync directory
+    backup_process = subprocess.run(['sshpass', '-p', password, 'rsync', '-avz', microSWIFT_data_dir,'{0}@{1}:{2}'.format(username, bigwaves, backup_dir)]) 
+    backup_process_rc = backup_process.returncode
+    if backup_process_rc == 0:
+        print('Data was successfully synced from local machine to bigwaves')
+    else:
+        print('Data was not successfully synced')
+
+# Run this main function as a script
 if __name__ == '__main__':
     main()
