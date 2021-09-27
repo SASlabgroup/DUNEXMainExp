f308a74f1910c7f203bc12e62c23c9f6610871bf
edwinrainville <erainvil@uw.edu>
2021-09-27 11:39:09 -0700

    Added pullDUNEXData.py

---
+++ tools/pullDUNEXData.py
@@ -0,0 +1,12 @@
+# pullDUNEXData.py 
+def main():
+    '''
+    @edwinrainville
+
+    Description: Pulls microSWIFT data from the backed up copy to your local machine and updates anything on your local machine
+
+    '''
+
+
+if __name__ == '__main__':
+    main()
