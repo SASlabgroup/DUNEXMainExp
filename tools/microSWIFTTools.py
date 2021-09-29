f3ac052298cbced20a042f160fa3594d28e85471
edwinrainville <erainvil@uw.edu>
2021-09-29 12:55:27 -0700

    added colorbar to missionMap()

--- tools/microSWIFTTools.py
+++ tools/microSWIFTTools.py
@@ -2,8 +2,9 @@
 # Import modules
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy.signal
 import netCDF4 as nc
+import cftime
+import datetime
 
 
 def missionMap(mission_num, mission_dir_path, mission_nc_path):
@@ -18,13 +19,21 @@ def missionMap(mission_num, mission_dir_path, mission_nc_path):
     microSWIFTs_on_mission = list(mission_dataset.groups.keys())
 
     # Create map of all drift tracks during mission
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(8,6))
     ax.set_xlabel('Longitude')  
     ax.set_ylabel('Latitude')
     fig.autofmt_xdate()
     for microSWIFT in microSWIFTs_on_mission:
-        ax.scatter(mission_dataset[microSWIFT]['GPS']['lon'][:], mission_dataset[microSWIFT]['GPS']['lat'][:], c=mission_dataset[microSWIFT]['GPS']['time'][:])
-
+        # Plot the lat-lon map with color coded points in time
+        map = ax.scatter(mission_dataset[microSWIFT]['GPS']['lon'][:], mission_dataset[microSWIFT]['GPS']['lat'][:], c=mission_dataset[microSWIFT]['GPS']['time'][:])
+    
+    # Set colorbar and figure properties
+    cbar = fig.colorbar(map, ax=ax)
+    cbar.ax.set_ylabel('Time [UTC]')
+    time_datetimes = cftime.num2pydate(mission_dataset[microSWIFT]['GPS']['time'][:],units=mission_dataset[microSWIFT]['GPS']['time'].units, calendar=mission_dataset[microSWIFT]['GPS']['time'].calendar)
+    cbar.ax.set_yticks([mission_dataset[microSWIFT]['GPS']['time'][0], mission_dataset[microSWIFT]['GPS']['time'][-1]])
+    cbar.ax.set_yticklabels([time_datetimes[0], time_datetimes[-1]])
+    plt.tight_layout()
     figure_path = mission_dir_path + 'mission_{}_map.png'.format(mission_num)
     plt.savefig(figure_path)
     return figure_path
