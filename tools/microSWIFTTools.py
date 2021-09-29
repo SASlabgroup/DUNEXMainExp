85f0a695cfca96aed3f52ea72151cb10031235f4
edwinrainville <erainvil@uw.edu>
2021-09-29 13:52:37 -0700

    Corrected all colorbar labels for time in the missionMap()

--- tools/microSWIFTTools.py
+++ tools/microSWIFTTools.py
@@ -23,16 +23,27 @@ def missionMap(mission_num, mission_dir_path, mission_nc_path):
     ax.set_xlabel('Longitude')  
     ax.set_ylabel('Latitude')
     fig.autofmt_xdate()
+
+    # Sort time labels 
+    min_time_label = mission_dataset[microSWIFTs_on_mission[0]]['GPS']['time'][0]
+    max_time_label = mission_dataset[microSWIFTs_on_mission[0]]['GPS']['time'][-1]
+
     for microSWIFT in microSWIFTs_on_mission:
         # Plot the lat-lon map with color coded points in time
         map = ax.scatter(mission_dataset[microSWIFT]['GPS']['lon'][:], mission_dataset[microSWIFT]['GPS']['lat'][:], c=mission_dataset[microSWIFT]['GPS']['time'][:])
+        # reset time labels for colormap 
+        if mission_dataset[microSWIFT]['GPS']['time'][0] < min_time_label:
+            min_time_label = mission_dataset[microSWIFT]['GPS']['time'][0]
+        if mission_dataset[microSWIFT]['GPS']['time'][-1] > max_time_label:
+            max_time_label = mission_dataset[microSWIFT]['GPS']['time'][-1]
     
     # Set colorbar and figure properties
-    cbar = fig.colorbar(map, ax=ax)
-    cbar.ax.set_ylabel('Time [UTC]')
-    time_datetimes = cftime.num2pydate(mission_dataset[microSWIFT]['GPS']['time'][:],units=mission_dataset[microSWIFT]['GPS']['time'].units, calendar=mission_dataset[microSWIFT]['GPS']['time'].calendar)
-    cbar.ax.set_yticks([mission_dataset[microSWIFT]['GPS']['time'][0], mission_dataset[microSWIFT]['GPS']['time'][-1]])
-    cbar.ax.set_yticklabels([time_datetimes[0], time_datetimes[-1]])
+    cbar = fig.colorbar(map, ax=ax, ticks=[min_time_label, max_time_label])
+    map.set_clim([min_time_label, max_time_label])
+    cbar.ax.set_xlabel('Time [UTC]')
+    time_labels = cftime.num2pydate([min_time_label, max_time_label],units=mission_dataset[microSWIFT]['GPS']['time'].units, calendar=mission_dataset[microSWIFT]['GPS']['time'].calendar)
+    time_labels = [time_labels[0].strftime('%Y-%m-%d %H:%M'), time_labels[1].strftime('%Y-%m-%d %H:%M')]
+    cbar.ax.set_yticklabels(time_labels, rotation=0, va='center')
     plt.tight_layout()
     figure_path = mission_dir_path + 'mission_{}_map.png'.format(mission_num)
     plt.savefig(figure_path)
