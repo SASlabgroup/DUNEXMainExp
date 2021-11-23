914766fb7a8ccae845aa4710a2a3de09be85f255
edwinrainville <erainvil@uw.edu>
2021-11-22 16:19:12 -0800

    Added missionView function to view the mission dataset

---
+++ tools/missionView.py
@@ -0,0 +1,33 @@
+## missionView.py
+# Import Packages
+import numpy as np
+import netCDF4 as nc
+
+def main():
+    '''
+    @edwinrainville
+
+    Description: A quick view of all data in a mission netCDF.
+
+    Parameters:
+        mission_nc: file name of mission netCDF file
+
+    Returns:
+        Drift tracks: Scatter plots of x and y position of the microSWIFT
+
+        Accelerations:
+
+        Magnetometers: 
+
+        Gyroscope: 
+
+        Click Through each microSWIFT in a mission 
+    
+    '''
+    # Define 
+    print('yay this is the missionView function')
+
+
+
+if __name__=='__main__':
+    main()
