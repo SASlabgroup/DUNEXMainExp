5e0bb39571bca42f05f23d5394f0730e34541ab0
edwinrainville <erainvil@uw.edu>
2021-12-04 14:57:16 -0800

    Added missionView.py

--- tools/missionView.py
+++ tools/missionView.py
@@ -4,8 +4,9 @@ import numpy as np
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import cftime
+import matplotlib.backends.backend_pdf
 
-def main(mission_nc_path, mission_num):
+def main(mission_nc_path=None, mission_num=None):
     '''
     @edwinrainville
 
@@ -26,88 +27,89 @@ def main(mission_nc_path, mission_num):
         Click Through each microSWIFT in a mission 
     
     '''
+    # User Inputs
+    if mission_nc_path == None:
+        mission_nc_path = input('Enter path to mission netCDF:')
+    
+    if mission_num == None:
+        mission_num = int(input('Enter mission number:'))
+
     # Create dataset object from the netCDF path
     mission_dataset = nc.Dataset(mission_nc_path, mode='r')
-    
+
+    # Define time from netCDF
+    time = cftime.num2pydate(mission_dataset['time'][:], calendar=mission_dataset['time'].calendar, units=mission_dataset['time'].units)
+
+    # Set up pdf to save figures to for each microSWIFT
+    pdf_name = '../buildDataset/missionViews/mission_{}.pdf'.format(mission_num)
+    pdf = matplotlib.backends.backend_pdf.PdfPages(pdf_name)
+
     # Get list of all microSWIFTs on the mission
     microSWIFTs_on_mission = list(mission_dataset.groups.keys())
 
-    # Create map of all drift tracks during mission
-    fig, ax = plt.subplots(figsize=(8,6))
-    ax.set_xlabel('FRF X Location [meters]')  
-    ax.set_ylabel('FRF Y Location [meters]')
+    # Loop through all microSWIFTs on mission to build the mission View
+    for microSWIFT_num in np.arange(len(microSWIFTs_on_mission)):
 
-    # Add the FRF Bathymetry to the map 
-    # Data from September 28th, 2021
-    bathy_url = 'https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/geomorphology/DEMs/surveyDEM/data/FRF_geomorphology_DEMs_surveyDEM_20210928.nc'
-    bathy_dataset = nc.Dataset(bathy_url)
-    # Create grid from coordinates
-    xFRF_grid, yFRF_grid = np.meshgrid(bathy_dataset['xFRF'][:],bathy_dataset['yFRF'][:])
-    bathy = bathy_dataset['elevation'][0,:,:]
-    ax.contourf(xFRF_grid, yFRF_grid, bathy, cmap='gray')
+        # Create map of drift tracks for the microSWIFT
+        fig = plt.figure()
+        fig.set_size_inches(8.5, 11)
+        ax1 = fig.add_subplot(1,2,1)
+        ax1.set_xlabel('FRF X Location [meters]')  
+        ax1.set_ylabel('FRF Y Location [meters]')
 
-    # # Sort time labels 
-    # initial_values_set = False
-    # while initial_values_set == False:
-    #     for microSWIFT in microSWIFTs_on_mission:
-    #         if 'GPS' in list(mission_dataset[microSWIFT].groups.keys()):
-    #             if 'time' in list(mission_dataset[microSWIFT]['GPS'].variables):
-    #                 # Set initial time labels
-    #                 min_time_label = mission_dataset[microSWIFT]['GPS']['time'][0]
-    #                 max_time_label = mission_dataset[microSWIFT]['GPS']['time'][-1]
+        # Add the FRF Bathymetry to the map 
+        # Data from September 28th, 2021
+        bathy_url = 'https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/geomorphology/DEMs/surveyDEM/data/FRF_geomorphology_DEMs_surveyDEM_20210928.nc'
+        bathy_dataset = nc.Dataset(bathy_url)
+        # Create grid from coordinates
+        xFRF_grid, yFRF_grid = np.meshgrid(bathy_dataset['xFRF'][:],bathy_dataset['yFRF'][:])
+        bathy = bathy_dataset['elevation'][0,:,:]
+        ax1.contourf(xFRF_grid, yFRF_grid, bathy, cmap='gray')
 
-    #                 # Sort x and y locations for map  
-    #                 min_x = np.min(mission_dataset[microSWIFT]['GPS']['xFRF'])
-    #                 max_x = np.max(mission_dataset[microSWIFT]['GPS']['xFRF'])
-    #                 min_y = np.min(mission_dataset[microSWIFT]['GPS']['yFRF'])
-    #                 max_y = np.max(mission_dataset[microSWIFT]['GPS']['yFRF'])
-    #                 initial_values_set = True
-    #             else:
-    #                 continue
-    #         else:
-    #             continue
-                
-    # for microSWIFT in microSWIFTs_on_mission:
-    #     # Compute local coordinates from each lat-lon series
-    #     if 'GPS' in list(mission_dataset[microSWIFT].groups.keys()):
-    #         if 'lat' and 'lon' in list(mission_dataset[microSWIFT]['GPS'].variables):
-    #             # Plot the lat-lon map with color coded points in time
-    #             map = ax.scatter(mission_dataset[microSWIFT]['GPS']['xFRF'], mission_dataset[microSWIFT]['GPS']['yFRF'], c=mission_dataset[microSWIFT]['GPS']['time'][:], cmap='plasma')
-    #             # reset time labels for colormap 
-    #             if mission_dataset[microSWIFT]['GPS']['time'][0] < min_time_label:
-    #                 min_time_label = mission_dataset[microSWIFT]['GPS']['time'][0]
-    #             if mission_dataset[microSWIFT]['GPS']['time'][-1] > max_time_label:
-    #                 max_time_label = mission_dataset[microSWIFT]['GPS']['time'][-1]
-    #             # Set max and min position
-    #             if np.min(x) < min_x:
-    #                 min_x = np.min(x)
-    #             if np.max(x) > max_x:
-    #                 max_x = np.max(x)
-    #             if np.min(y) < min_y:
-    #                 min_y = np.min(y)
-    #             if np.max(y) > max_y:
-    #                 max_y = np.max(y)
-                
-    #             # Figure Properties
-    #             time_labels = cftime.num2pydate([min_time_label, max_time_label],units=mission_dataset[microSWIFT]['GPS']['time'].units, calendar=mission_dataset[microSWIFT]['GPS']['time'].calendar)
-            
-    # # Set colorbar and figure properties
-    # cbar = fig.colorbar(map, ax=ax, ticks=[min_time_label, max_time_label])
-    # map.set_clim([min_time_label, max_time_label])
-    # ax.set_xlim([min_x, max_x])
-    # ax.set_ylim([min_y, max_y])
-    # cbar.ax.set_xlabel('Time [UTC]')
-    # time_labels = [time_labels[0].strftime('%Y-%m-%d %H:%M'), time_labels[1].strftime('%Y-%m-%d %H:%M')]
-    # cbar.ax.set_yticklabels(time_labels, rotation=0, va='center')
+        # Plot the microSWIFT drift track on bathymetry
+        x = mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['xFRF'][:]
+        y = mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['yFRF'][:]
+        ax1.scatter(x, y, color='g')
+        ax1.set_xlim([np.min(x)-100, np.max(x)+100])
+        ax1.set_ylim([np.min(y)-100, np.max(y)+100])
+        ax1.set_title('Drift Track')
 
-    plt.tight_layout()
-    ax.set_aspect('equal')
-    figure_path = './mission_{}_map.png'.format(mission_num)
-    plt.savefig(figure_path)
+        # Plot the accelerations
+        ax2 = fig.add_subplot(3,2,2)
+        ax2.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['accel_x'][:], color='g', label='X')
+        ax2.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['accel_y'][:], color='b', label='Y')
+        ax2.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['accel_z'][:], color='k', label='Z')
+        ax2.set_xlabel('Time')
+        ax2.set_ylabel('Acceleration [m/s^2]')
+        ax2.legend(bbox_to_anchor=(0,1.04,1,0.2), loc="lower left", mode="expand", borderaxespad=0, ncol=3)
+
+        # Plot the Gyroscope
+        ax3 = fig.add_subplot(3,2,4)
+        ax3.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['gyro_x'][:], color='g', label='X')
+        ax3.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['gyro_y'][:], color='b', label='Y')
+        ax3.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['gyro_z'][:], color='k', label='Z')
+        ax3.set_xlabel('Time')
+        ax3.set_ylabel('Rotations [degrees/sec]')
+        ax3.set_title('Gyroscope')
+
+        # Plot the Magnetometer
+        ax4 = fig.add_subplot(3,2,6)
+        ax4.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['mag_x'][:], color='g', label='X')
+        ax4.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['mag_y'][:], color='b', label='Y')
+        ax4.plot(time, mission_dataset[microSWIFTs_on_mission[microSWIFT_num]]['mag_z'][:], color='k', label='Z')
+        ax4.set_xlabel('Time')
+        ax4.set_ylabel('Magnetometer [uTeslas]')
+        ax4.set_title('Magnetometer')
+
+        # Figure Properties 
+        plt.tight_layout()
+        ax1.set_aspect('equal')
+        pdf.savefig(fig)
 
     # Close the dataset
     mission_dataset.close()
     bathy_dataset.close()
+    pdf.close()
 
 if __name__=='__main__':
     main()
