4be15db36243da3db9c3312cd907cc6d930a0cb7
edwinrainville <erainvil@uw.edu>
2021-12-04 17:07:15 -0800

    moved buildMissionNCs to the tools directory

--- tools/buildMissionNCs.py
+++ tools/buildMissionNCs.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import sys
 import datetime
+import logging
 
 # Import DUNEX Tools
 sys.path.append('..')
@@ -16,6 +17,9 @@ def main():
     Description: Build and clean the basic mission datasets.
     TODO: Finish writing description
     '''
+    # Set up logger
+    logging.basicConfig(filename='../microSWIFT_data/cleanedDataset/build_dataset.log', encoding='utf-8', level=logging.DEBUG)
+
     # Define number of missions
     number_of_missions = 81
     
@@ -35,14 +39,17 @@ def main():
     dunex_xlsx = pd.read_excel(metadata_filename)
 
     # Loop through each mission and build netCDF files
-    for mission_num in np.arange(20, number_of_missions):
+    for mission_num in np.arange(1, number_of_missions):
         # Get start and end times
         start_time = datetime.datetime.fromisoformat(dunex_xlsx['Start Time'].iloc[mission_num])
         end_time = datetime.datetime.fromisoformat(dunex_xlsx['End Time'].iloc[mission_num])
 
         # Build a netCDF for the mission that has only raw time values read in 
-        print('building mission {}'.format(mission_num))
+        logging.info('building mission {}'.format(mission_num))
+        tic = datetime.datetime.utcnow()
         mission_nc_path = buildBasicMissionNC.main(mission_num=mission_num)
+        toc = datetime.datetime.utcnow()
+        logging.info('Time to build mission {0} was {1}}'.format(mission_num, toc-tic))
 
 if __name__=='__main__':
     main()
