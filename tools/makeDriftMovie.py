630b6355553d13774c51eadacbdaab5b8ac83a33
edwinrainville <erainvil@uw.edu>
2021-12-07 16:45:38 -0800

    Added function to make a movie of each drift track

---
+++ tools/makeDriftMovie.py
@@ -0,0 +1,11 @@
+# Make Drift Movie from Mission netCDF
+
+def main():
+    '''
+    @edwinrainville
+
+    Description: This function will be used to plot a movie of the drift tracks from the DUNEX experiment.
+    '''
+    
+if __name__ == '__main__':
+    main()
