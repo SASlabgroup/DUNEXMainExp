06e27cc8474f736f0d12f1cdbe78391b7819856c
edwinrainville <erainvil@uw.edu>
2021-12-10 10:33:00 -0800

    updated buildMissionNCs.py batch script to include a start and stop

--- tools/buildMissionNCs.py
+++ tools/buildMissionNCs.py
@@ -8,6 +8,9 @@ import logging
 # Import DUNEX Tools
 sys.path.append('..')
 from tools import buildBasicMissionNC
+from tools import cleanDUNEXMissions
+from tools import missionView
+from tools import makeDriftMovie
 
 def main():
     '''
@@ -17,19 +20,52 @@ def main():
     TODO: Finish writing description
     '''
     # Set up logger
-    logging.basicConfig(filename='../microSWIFT_data/cleanedDataset/build_dataset.log', encoding='utf-8', level=logging.DEBUG)
+    logging.basicConfig(filename='../microSWIFT_data/cleanedDataset/build_dataset.log', filemode='w', encoding='utf-8')
 
     # Define number of missions
-    number_of_missions = 81
+    mission_start = 7
+    mission_end = 81
+    # number_of_missions = 81
+    # number_of_missions = 5
+
+    # start timer for entire dataset build
+    start_build = datetime.datetime.utcnow()
     
     # Loop through each mission and build netCDF files
-    for mission_num in np.arange(1, number_of_missions+1):
+    for mission_num in np.arange(mission_start, mission_end+1):
         # Build a netCDF for the mission that has only raw time values read in 
         logging.info('building mission {}'.format(mission_num))
         tic = datetime.datetime.utcnow()
         mission_nc_path = buildBasicMissionNC.main(mission_num=mission_num)
         toc = datetime.datetime.utcnow()
         logging.info('Time to build mission {0} was {1}'.format(mission_num, toc-tic))
+
+        # Clean the dataset
+        logging.info('cleaning mission {}'.format(mission_num))
+        tic = datetime.datetime.utcnow()
+        cleanDUNEXMissions.main(mission_num=mission_num)
+        toc = datetime.datetime.utcnow()
+        logging.info('Time to clean mission {0} was {1}'.format(mission_num, toc-tic))
+
+        # Plot the mission dataset to view
+        logging.info('plotting mission {} dataset'.format(mission_num))
+        tic = datetime.datetime.utcnow()
+        missionView.main(mission_num=mission_num)
+        toc = datetime.datetime.utcnow()
+        logging.info('Time to plot mission {0} was {1}'.format(mission_num, toc-tic))
+
+        # # Make Drift movie
+        # logging.info('making drfit movie for mission {} dataset'.format(mission_num))
+        # tic = datetime.datetime.utcnow()
+        # makeDriftMovie.main(mission_num=mission_num)
+        # toc = datetime.datetime.utcnow()
+        # logging.info('Time to make drift movie for mission {0} was {1}'.format(mission_num, toc-tic))
+
+    # Finish timing the build
+    end_build = datetime.datetime.utcnow()
+    logging.info('Dataset was built successfully')
+    logging.info('Total time to build Dataset = {}'.format(end_build - start_build))
+
 
 if __name__=='__main__':
     main()
