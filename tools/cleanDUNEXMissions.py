42bd21d2608b1596c874fc1f28afeeb1385c58e5
edwinrainville <erainvil@uw.edu>
2021-12-10 16:53:59 -0800

    Added individual masking for microSWIFTs to cleanDUNEXMissions.py

--- tools/cleanDUNEXMissions.py
+++ tools/cleanDUNEXMissions.py
@@ -3,6 +3,7 @@ import numpy as np
 import logging
 import netCDF4 as nc
 import matplotlib.pyplot as plt
+import cftime
 
 def computeBeachLocation():
     '''
@@ -21,9 +22,12 @@ def computeBeachLocation():
     elevation = bathy['elevation'][0,:,:]
 
     # Load in water level data
-    # TODO: replace these values with true waterLavel Data
-    time = np.arange(100)
-    waterLevel_series = 5 * np.sin(((2*np.pi)/10) * time)  # units are meters
+    # TODO: replace these values with true waterLavel Data instead of predicted once it is posted
+    url = 'https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/oceanography/waterlevel/eopNoaaTide/2021/FRF-ocean_waterlevel_eopNoaaTide_202110.nc'
+    waterLevel_dataset = nc.Dataset(url)
+    time = cftime.num2pydate(waterLevel_dataset['time'][:], calendar=waterLevel_dataset['time'].calendar, units=waterLevel_dataset['time'].units)
+    # waterLevel_series = waterLevel_dataset['waterLevel'][:]
+    waterLevel_series = waterLevel_dataset['predictedWaterLevel'][:]
 
     # Define empty list for max x values
     beach_x_series = []
@@ -60,11 +64,49 @@ def individualMissionMicroSWIFTMask(mission_num = None):
     Returns the indivudal mask for each microSWIFT in each mission which are developed from visual inspection - each mask has notes as to how it was developed
     
     '''
+    # Define mission Number
+    if mission_num == None:
+        mission_num = int(input('Enter Mission Number: '))
+
+    # Function to initialize mask dictionary
+    def init_mask_dict(mission_num=mission_num):
+        # define mission netCF
+        mission_dataset = nc.Dataset('../microSWIFT_data/cleanedDataset/mission_{}.nc'.format(mission_num))
+
+        # define list of microSWIFTs on mission 
+        microSWIFTs_on_mission = list(mission_dataset.groups.keys())
+
+        # Initialize mask 
+        mission_masks = {microSWIFT: None for microSWIFT in microSWIFTs_on_mission}
+
+        # Return the initialized mask
+        return mission_masks
+    
+    # Mission 1 Individual mask 
+    if mission_num == 1:
+        # Intialize mask 
+        mission_masks = init_mask_dict(mission_num=mission_num)
+
+        # Append Mask with indices to mask
 
 
+     # Mission 20 Individual mask 
+    if mission_num == 20:
+        # Intialize mask 
+        mission_masks = init_mask_dict(mission_num=mission_num)
 
+        # microSWIFT_31
+        # description: There is a section of all imu data at the end of the time series that values are not fluctuating signifying that it is on the beach but not masked by the 
+        mission_masks['microSWIFT_31'] = slice(9000, -1)
 
-def main(mission_num = None):
+        # microSWIFT_21
+        # description: There is a very small section of data at the very end of the time series that it most likely is being masked from the beach but maybe moved back across the beach mask briefly 
+        mission_masks['microSWIFT_21'] = slice(10000, -1)
+        
+    # Return the mission mask dictionary
+    return mission_masks
+
+def main(mission_num=None):
     '''
     @edwinrainville
 
@@ -75,15 +117,14 @@ def main(mission_num = None):
     logging.basicConfig(filename='../microSWIFT_data/cleanedDataset/build_dataset.log', encoding='utf-8', level=logging.DEBUG)
 
      # Compute beach 
-    beach_x_series, time = computeBeachLocation()
+    beach_x_series, beach_x_time = computeBeachLocation()
 
     # Load in each mission netCDF and apply masks 
     data_dir = '../microSWIFT_data/cleanedDataset/'
 
     # Define mission Number
     if mission_num == None:
-        mission_num = 20
-        # mission_num = int(input('Enter Mission Number: '))
+        mission_num = int(input('Enter Mission Number: '))
 
     # Convert mission number to a path to the file
     mission_nc = 'mission_{}.nc'.format(mission_num)
@@ -93,7 +134,9 @@ def main(mission_num = None):
     mission_dataset = nc.Dataset(mission_nc_path, mode='a')
 
     # From start time find the correct beach_x value 
-    beach_x_during_mission = beach_x_series[0]
+    mission_start = cftime.num2pydate(mission_dataset['time'][0], calendar=mission_dataset['time'].calendar, units=mission_dataset['time'].units)
+    start_time_closest_index = int(np.argmin(np.abs(beach_x_time - mission_start)))
+    beach_x_during_mission = beach_x_series[start_time_closest_index]
 
     # For each microSWIFT on mission
     microSWIFTs_on_mission = list(mission_dataset.groups.keys())
@@ -109,6 +152,16 @@ def main(mission_num = None):
         for variable in microSWIFT_variables:
             mission_dataset[microSWIFT][variable][beach_mask_ind] = np.ma.masked
 
+    # Mask each individual microSWIFT on each mission
+    mission_mask_dict = individualMissionMicroSWIFTMask(mission_num=mission_num)
+    for microSWIFT in list(mission_mask_dict.keys()):
+        microSWIFT_mask = mission_mask_dict[microSWIFT]
+        if microSWIFT_mask != None:
+            # Mask all indices where the microSWIFT is on the beach
+            microSWIFT_variables = list(mission_dataset[microSWIFT].variables.keys())
+            for variable in microSWIFT_variables:
+                mission_dataset[microSWIFT][variable][microSWIFT_mask] = np.ma.masked
+    
     # Close the dataset
     mission_dataset.close()
 
