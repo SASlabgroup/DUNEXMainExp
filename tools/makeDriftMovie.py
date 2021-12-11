64e7ed556394abb16994e8cf53bfbcb2ef6fbd17
edwinrainville <erainvil@uw.edu>
2021-12-10 16:55:07 -0800

    Able to make drift track gif of each mission

--- tools/makeDriftMovie.py
+++ tools/makeDriftMovie.py
@@ -1,4 +1,5 @@
 # Make Drift Movie from Mission netCDF
+from matplotlib.markers import MarkerStyle
 import numpy as np
 import netCDF4 as nc
 import matplotlib.pyplot as plt
@@ -24,10 +25,6 @@ def main(mission_num=None):
     # Define time from netCDF
     time = cftime.num2pydate(mission_dataset['time'][:], calendar=mission_dataset['time'].calendar, units=mission_dataset['time'].units)
 
-    # Get list of all microSWIFTs on the mission
-    microSWIFTs_on_mission = list(mission_dataset.groups.keys())
-    microSWIFT =microSWIFTs_on_mission[0]
-
     # Add the FRF Bathymetry to the map 
     # Data from September 28th, 2021
     bathy_url = 'https://chlthredds.erdc.dren.mil/thredds/dodsC/frf/geomorphology/DEMs/surveyDEM/data/FRF_geomorphology_DEMs_surveyDEM_20210928.nc'
@@ -38,7 +35,6 @@ def main(mission_num=None):
 
     # Create map of drift tracks for the microSWIFT
     fig = plt.figure()
-    # fig.set_size_inches(8.5, 11)
     ax = plt.subplot(1,1,1)
 
     def init_tracks():
@@ -46,13 +42,27 @@ def main(mission_num=None):
         ax.set_xlabel('FRF X Location [meters]')
         ax.set_ylabel('FRF Y Location [meters]')
         ax.contourf(xFRF_grid, yFRF_grid, bathy, cmap='gray')
-        
+
     def update_tracks(index):
-        ax.scatter(mission_dataset[microSWIFT]['xFRF'][index], mission_dataset[microSWIFT]['yFRF'][index])
+        ax.clear()
+        ax.set_xlabel('FRF X Location [meters]')
+        ax.set_ylabel('FRF Y Location [meters]')
+        ax.contourf(xFRF_grid, yFRF_grid, bathy, cmap='gray')
+
+         # Get list of all microSWIFTs on the mission
+        microSWIFTs_on_mission = list(mission_dataset.groups.keys())
+        for microSWIFT in microSWIFTs_on_mission:
+            ax.plot(mission_dataset[microSWIFT]['xFRF'][0], mission_dataset[microSWIFT]['yFRF'][0], color='r', marker='o')
+            ax.plot(mission_dataset[microSWIFT]['xFRF'][:index], mission_dataset[microSWIFT]['yFRF'][:index], color='k')
+            ax.plot(mission_dataset[microSWIFT]['xFRF'][index], mission_dataset[microSWIFT]['yFRF'][index], color='g', marker='o')
+        
+        # Add title with time
+        ax.set_title('Mission {0} - {1}'.format(mission_num, time[index]))
 
     # Creating the Animation object
-    anim = animation.FuncAnimation(fig, update_tracks, frames=np.arange(0, len(time)), init_func=init_tracks, interval=20)
-    anim.save('drifttrack.mp4', dpi=150, fps=30, writer='ffmpeg')
+    data_skip = 200
+    anim = animation.FuncAnimation(fig, update_tracks, frames=np.arange(0, len(time), data_skip), init_func=init_tracks)
+    anim.save('../microSWIFT_data/cleanedDataset/Figures/mission_{}_drift.gif'.format(mission_num), dpi=150, fps=10, writer='pillow')
 
     # Close dataset and figures
     mission_dataset.close()
